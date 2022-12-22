892de02ab3a84febd877d90a77c34198ab7f3f51
santos <usuario@pc21-052>
2022-12-22 09:29:38 +0100

    Clase Tarea actualizada

--- SCRUM/src/Clases/Tarea.java
+++ SCRUM/src/Clases/Tarea.java
@@ -1,5 +1,28 @@
 package Clases;
 
-public class Tarea {
+import java.util.ArrayList;
 
+public class Tarea {
+	private String nombreTarea;
+	private ArrayList<Integer> esfuerzo = new ArrayList<Integer>();
+	
+	public Tarea(String nT,int esf_inicial) {
+		this.nombreTarea = nT;
+		this.esfuerzo.add(0, esf_inicial);
+	}
+	
+	public void añadirEsfuerzo(int esf, int dia) {
+		if(esfuerzo.size()<dia-1) {
+			for(int i=esfuerzo.size();i<dia-1;i++) {
+				esfuerzo.add(esfuerzo.get(i-1));
+			}
+		}
+		esfuerzo.add(esf);
+	}
+	public ArrayList<Integer> getEsfuerzo(){
+		return esfuerzo;
+	}
+	public String getNombre() {
+		return nombreTarea;
+	}
 }
