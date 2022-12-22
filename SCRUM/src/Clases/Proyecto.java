f095fcd390c90b6de8b16c0e8475234b0896554b
juan <usuario@pc21-053>
2022-12-22 09:33:10 +0100

    clase proyecto

--- SCRUM/src/Clases/Proyecto.java
+++ SCRUM/src/Clases/Proyecto.java
@@ -1,5 +1,40 @@
 package Clases;
+import java.util.ArrayList;
 
 public class Proyecto {
-
+	private ArrayList<Trabajador> trabajadores=new ArrayList<Trabajador>();
+	private ArrayList<Tarea> tareas=new ArrayList<Tarea>();
+	private int duracion, sprint;
+	private String nombre;
+	
+	public Proyecto(String nombre, int duracion, int sprint) {
+		this.nombre=nombre;
+		this.duracion=duracion;
+		this.sprint=sprint;
+	}
+	
+	
+	public ArrayList<Trabajador> getTrabajadores() {
+		return trabajadores;
+	}
+	
+	public ArrayList<Tarea> getTareas() {
+		return tareas;
+	}
+	
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public int getSprint() {
+		return sprint;
+	}
+	public void setSprint(int sprint) {
+		this.sprint = sprint;
+	}
+	
+	
+	
 }
