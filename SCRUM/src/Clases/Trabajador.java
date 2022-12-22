d9b809053dfafa856749eacdca7664fe8c123f38
Juan <usuario@pc21-053>
2022-12-22 09:04:55 +0100

    clase trabajador

--- SCRUM/src/Clases/Trabajador.java
+++ SCRUM/src/Clases/Trabajador.java
@@ -1,5 +1,13 @@
 package Clases;
 
 public class Trabajador {
-
+	String nombre;
+	
+	public Trabajador(String n) {
+		this.nombre=n;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
 }
