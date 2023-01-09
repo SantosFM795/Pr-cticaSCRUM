747d6cfbc6b6ee8d708fe1f9489cb4d7ccbb42d2
Juan <Kamisama@GOD>
2023-01-09 02:33:36 +0100

    desarrollo de IActualizarEstado (falta probar y corregir)

--- SCRUM/src/Interfaz/IActualizarEstado.java
+++ SCRUM/src/Interfaz/IActualizarEstado.java
@@ -1,6 +1,7 @@
 package Interfaz;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,11 +15,19 @@ import javax.swing.JMenu;
 import javax.swing.JCheckBoxMenuItem;
 import java.awt.Choice;
 import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class IActualizarEstado extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField tHoras;
+	private int posicionTarea;
+	private int posicionDia;
 
 
 	/**
@@ -47,10 +56,12 @@ public class IActualizarEstado extends JFrame {
 		contentPane.add(lblNewLabel_2);
 		
 		Choice cTarea = new Choice();
+		
 		cTarea.setBounds(199, 46, 237, 20);
 		contentPane.add(cTarea);
 		
 		Choice cDia = new Choice();
+		
 		cDia.setBounds(199, 150, 97, 20);
 		contentPane.add(cDia);
 		
@@ -58,6 +69,66 @@ public class IActualizarEstado extends JFrame {
 		tHoras.setBounds(199, 228, 46, 20);
 		contentPane.add(tHoras);
 		tHoras.setColumns(10);
+		
+		JLabel lError = new JLabel("*Este campo no puede estar vacío");
+		lError.setForeground(Color.RED);
+		lError.setBounds(199, 259, 189, 14);
+		contentPane.add(lError);
+		
+		JButton bAplicar = new JButton("Aplicar");
+		
+		bAplicar.setBounds(455, 306, 89, 23);
+		contentPane.add(bAplicar);
+		lError.setVisible(false);
+		
+		for (int i = 0; i < p.getTareas().size(); i++) {
+			cTarea.add(p.getTareas().get(i).getNombre());
+		}
+		
+		posicionTarea=cTarea.getSelectedIndex();
+		posicionDia=Integer.parseInt(cDia.getSelectedItem());
+		
+		cTarea.getItemCount();
+		for (int i = p.getTareas().get(posicionTarea).getEsfuerzo().size()+1; i < p.getDuracion()  ; i++) {
+			cDia.add(""+i);
+		}
+		
+		
+		
+		cTarea.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				posicionTarea=cTarea.getSelectedIndex();
+			}
+		});
+		
+		cDia.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				posicionDia=Integer.parseInt(cDia.getSelectedItem());
+			}
+		});
+		
+		bAplicar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(tHoras.getText().equalsIgnoreCase("")) {
+					lError.setVisible(true);
+				}else {
+					lError.setVisible(false);
+					for(int i=p.getTareas().get(posicionTarea).getEsfuerzo().size(); i < posicionDia;i++) {
+						if(i==posicionDia) {
+							int valor=Integer.parseInt(tHoras.getText());
+							p.getTareas().get(posicionTarea).getEsfuerzo().add(valor);
+							if(valor==0) {
+								p.getTareas().get(posicionTarea).setEstado("Terminado");
+							}else {
+								p.getTareas().get(posicionTarea).setEstado("En curso");
+							}
+						}else {
+							p.getTareas().get(posicionTarea).getEsfuerzo().add(p.getTareas().get(posicionTarea).getEsfuerzo().get(i));
+						}
+					}
+				}
+			}
+		});
 		
 		
 	}
